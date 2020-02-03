6966d5ecfc61f1abdcc6ce1a6d8c1eff542db9f0
Souham Biswas <souham.biswas@outlook.com>
2020-02-04 01:17:56 +0530

    oidv4 faster rcnn working

--- detrakt/detector.py
+++ detrakt/detector.py
@@ -11,21 +11,24 @@ import time
 from glob import glob
 cwd = os.path.dirname(os.path.realpath(__file__))
 
+import cv2
+
 # Uncomment the following two lines if need to use the Tensorflow visualization_unitls
-#os.chdir(cwd+'/models')
-#from object_detection.utils import visualization_utils as vis_util
+# os.chdir(cwd+'/models')
+
+from object_detection.utils import visualization_utils as vis_util
 
 class CarDetector(object):
     def __init__(self):
 
         self.car_boxes = []
-        
         os.chdir(cwd)
+
+        # Tensorflow localization/detection model
+        # Single-shot-detection with mobile net architecture trained on COCO dataset
         
-        #Tensorflow localization/detection model
-        # Single-shot-dectection with mobile net architecture trained on COCO dataset
-        
-        detect_model_name = 'ssd_mobilenet_v1_coco_11_06_2017'
+        # detect_model_name = 'ssd_mobilenet_v1_coco_11_06_2017'
+        detect_model_name = 'faster_rcnn_inception_resnet_v2_atrous_oid_v4_2018_12_12'
         
         PATH_TO_CKPT = detect_model_name + '/frozen_inference_graph.pb'
         
@@ -46,13 +49,12 @@ class CarDetector(object):
                
             self.sess = tf.Session(graph=self.detection_graph, config=config)
             self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-              # Each box represents a part of the image where a particular object was detected.
             self.boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
               # Each score represent how level of confidence for each of the objects.
               # Score is shown on the result image, together with the class label.
-            self.scores =self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
             self.classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-            self.num_detections =self.detection_graph.get_tensor_by_name('num_detections:0')
+            self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
     
     # Helper function to convert image into numpy array    
     def load_image_into_numpy_array(self, image):
@@ -65,8 +67,20 @@ class CarDetector(object):
         height, width = dim[0], dim[1]
         box_pixel = [int(box[0]*height), int(box[1]*width), int(box[2]*height), int(box[3]*width)]
         return np.array(box_pixel)       
-        
-    def get_localization(self, image, visual=False):  
+
+    def smart_resize(self, im, min_dim=600):
+        h, w, _ = im.shape
+        r = h / w
+        if h < w:
+            new_h = min_dim
+            new_w = int(np.clip(new_h * (1 / r), 0, 1024))
+        else:
+            new_w = min_dim
+            new_h = int(np.clip(new_w * r, 0, 1024))
+        im_resized = cv2.resize(im, (new_w, new_h))
+        return im_resized
+
+    def get_localization(self, image, visual=True):
         
         """Determines the locations of the cars in the image
 
@@ -77,48 +91,62 @@ class CarDetector(object):
             list of bounding boxes: coordinates [y_up, x_left, y_down, x_right]
 
         """
-        category_index={1: {'id': 1, 'name': u'person'},
-                        2: {'id': 2, 'name': u'bicycle'},
-                        3: {'id': 3, 'name': u'car'},
-                        4: {'id': 4, 'name': u'motorcycle'},
-                        5: {'id': 5, 'name': u'airplane'},
-                        6: {'id': 6, 'name': u'bus'},
-                        7: {'id': 7, 'name': u'train'},
-                        8: {'id': 8, 'name': u'truck'},
-                        9: {'id': 9, 'name': u'boat'},
-                        10: {'id': 10, 'name': u'traffic light'},
-                        11: {'id': 11, 'name': u'fire hydrant'},
-                        13: {'id': 13, 'name': u'stop sign'},
-                        14: {'id': 14, 'name': u'parking meter'}}  
-        
+        # category_index={1: {'id': 1, 'name': u'person'},
+        #                 2: {'id': 2, 'name': u'bicycle'},
+        #                 3: {'id': 3, 'name': u'car'},
+        #                 4: {'id': 4, 'name': u'motorcycle'},
+        #                 5: {'id': 5, 'name': u'airplane'},
+        #                 6: {'id': 6, 'name': u'bus'},
+        #                 7: {'id': 7, 'name': u'train'},
+        #                 8: {'id': 8, 'name': u'truck'},
+        #                 9: {'id': 9, 'name': u'boat'},
+        #                 10: {'id': 10, 'name': u'traffic light'},
+        #                 11: {'id': 11, 'name': u'fire hydrant'},
+        #                 13: {'id': 13, 'name': u'stop sign'},
+        #                 14: {'id': 14, 'name': u'parking meter'}}
+
+        label_map_data = open('../object_detection/data/oid_v4_label_map.pbtxt', 'r').readlines()
+        ids = []
+        names = []
+        category_index = {}
+        for l in label_map_data:
+            if 'id:' in l:
+                id = int(l.replace('id:', '').strip())
+                ids.append(id)
+            elif 'display_name:' in l:
+                name = l.replace('display_name:', '').strip().replace('"', '')
+                names.append(name)
+        for i in range(len(ids)):
+            category_index[ids[i]] = {'id': ids[i], 'name': names[i]}
+
+
         with self.detection_graph.as_default():
+              image = self.smart_resize(image)
               image_expanded = np.expand_dims(image, axis=0)
               (boxes, scores, classes, num_detections) = self.sess.run(
                   [self.boxes, self.scores, self.classes, self.num_detections],
                   feed_dict={self.image_tensor: image_expanded})
           
-              if visual == True:
+              if visual:
+                  im = image.copy()
                   vis_util.visualize_boxes_and_labels_on_image_array(
-                      image,
+                      im,
                       np.squeeze(boxes),
                       np.squeeze(classes).astype(np.int32),
                       np.squeeze(scores),
                       category_index,
-                      use_normalized_coordinates=True,min_score_thresh=.4,
+                      use_normalized_coordinates=True, min_score_thresh=.4,
                       line_thickness=3)
-    
-                  plt.figure(figsize=(9,6))
-                  plt.imshow(image)
-                  plt.show()  
+                  cv2.imwrite('p.png', im)
               
-              boxes=np.squeeze(boxes)
-              classes =np.squeeze(classes)
+              boxes = np.squeeze(boxes)
+              classes = np.squeeze(classes)
               scores = np.squeeze(scores)
     
               cls = classes.tolist()
               
               # The ID for car in COCO data set is 3 
-              idx_vec = [i for i, v in enumerate(cls) if ((v==3) and (scores[i]>0.3))]
+              idx_vec = [i for i, v in enumerate(cls) if v == 3 and scores[i] > 0.3]
               
               if len(idx_vec) ==0:
                   print('no detection!')
@@ -130,17 +158,12 @@ class CarDetector(object):
                       box = self.box_normal_to_pixel(boxes[idx], dim)
                       box_h = box[2] - box[0]
                       box_w = box[3] - box[1]
-                      ratio = box_h/(box_w + 0.01)
-                      
-                      if ((ratio < 0.8) and (box_h>20) and (box_w>20)):
+                      ratio = box_h / (box_w + 0.01)
+                      if ((ratio < 0.8) and (box_h / dim[0] > .026) and (box_w / dim[1] > .015)):
                           tmp_car_boxes.append(box)
                           print(box, ', confidence: ', scores[idx], 'ratio:', ratio)
-                         
                       else:
                           print('wrong ratio or wrong size, ', box, ', confidence: ', scores[idx], 'ratio:', ratio)
-                          
-                          
-                  
                   self.car_boxes = tmp_car_boxes
              
         return self.car_boxes
